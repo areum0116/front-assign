d97dab06e1548826b06691504ff97ce4ac2c3bab
areum <areum0116@naver.com>
2025-05-23 11:32:39 +0900

    lotto done

---
+++ lotto/app.js
@@ -0,0 +1,30 @@
+let numbers = [];
+
+const lottoBtn = document.getElementById("lottoBtn");
+const lottoNum = document.querySelectorAll(".lottoNum")
+
+function handleLottoBtnClick() {
+    numbers = [];
+    for(let i = 0; i < 6; i++) {
+        numbers.push(Math.floor(Math.random() * 45) + 1);
+    }
+    numbers.sort((a, b) => a - b);
+
+    for(let i = 0; i < 6; i++) {
+        lottoNum[i].innerText = numbers[i];
+        lottoNum[i].classList.remove("yellow", "blue", "red", "black", "green");
+        if(numbers[i] < 10) {
+            lottoNum[i].classList.add("yellow");
+        } else if(numbers[i] < 20) {
+            lottoNum[i].classList.add("blue");
+        } else if(numbers[i] < 30) {
+            lottoNum[i].classList.add("red");
+        } else if(numbers[i] < 40) {
+            lottoNum[i].classList.add("black");
+        } else {
+            lottoNum[i].classList.add("green");
+        }
+    }
+}
+
+lottoBtn.addEventListener("click", handleLottoBtnClick);
